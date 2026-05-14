dcee2f68130f9c35ea272aed5328a9ff21e7f0a7
marufhasanswe <marufhasanswe@gmail.com>
2026-05-14 17:04:07 +0600

    final commit

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
         <div className="max-w-7xl mx-auto px-6 text-white flex  justify-between">
           <div className="">
             <h1 className="text-4xl md:text-6xl font-bold mb-4 max-w-2xl">
-              Upgrade Your <span className="">Skill</span> Today
+              Upgrade Your <span className="">Skills</span> Today
             </h1>
             <p className="text-lg md:text-xl mb-6 max-w-xl text-gray-200">
               Master in-demand skills with expert-led courses, hands-on
